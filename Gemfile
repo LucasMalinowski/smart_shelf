b3575ba6291999cc53a6fb99b6a3c8992c66e829
lucasbrazau <lucasbrazau@fetch.ly>
2025-04-26 09:33:16 -0300

    Add Active Storage setup and image processing support
    Introduced Active Storage with required migrations, schema updates, and model associations for handling file attachments. Enabled `image_processing` gem for image transformations and integrated API-based image generation for `Item` model using OpenAI's DALL-E. Updated Gemfile, credentials, and related configurations for the new dependencies.

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem "pg", "~> 1.1"
 
 gem 'devise'
 
+gem 'faraday'
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
@@ -45,7 +46,7 @@ gem "tzinfo-data", platforms: %i[ windows jruby ]
 gem "bootsnap", require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
-# gem "image_processing", "~> 1.2"
+gem "image_processing", "~> 1.2"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,42 @@
 class Item < ApplicationRecord
   belongs_to :category
   has_many :user_items
+  has_one_attached :image
+
+  # after_create :create_image
+
+  def create_image
+    response = image_request
+    if response["created"].present?
+      image.attach(io: StringIO.new(Base64.decode64(response.dig('data').first.dig("b64_json"))),
+                   filename: "#{name.parameterize.underscore}.jpg",
+                   content_type: "image/jpeg")
+    else
+      Rails.logger.error "Failed to generate image for #{name}: #{response['error']}"
+    end
+  rescue => e
+    Rails.logger.error "Error attaching image for #{name}: #{e.message}"
+  end
+
+  def image_request(args: nil)
+    request = Faraday.post(
+      "https://api.openai.com/v1/images/generations",
+      {
+        prompt: "Generate a High Quality, white background, 4k image of '#{name}' (#{category.name}) in a realistic style to be used in a storage system.
+           The image should be clear and detailed, showcasing the item in a way that highlights its features.
+           The image should be suitable for use in a grocery list application.
+           #{args.nil? ? '' : args}
+        ",
+        model: 'dall-e-2',
+        size: '256x256',
+        response_format: 'b64_json',
+      }.to_json,
+      {
+        'Authorization' => "Bearer #{Rails.application.credentials.openai[:api_key]}",
+        'Content-Type' => 'application/json'
+      }
+    )
+
+    JSON.parse(request.body)
+  end
 end
